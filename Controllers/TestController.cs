dc703bdf4c8414aa3196d2df5a9f810025347861
Ryan Graute <rrgraute@gmail.com>
2018-03-22 12:57:47 +0100

    appsettings template file to hide database string

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,6 +14,7 @@ namespace project3data.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
+            System.Console.WriteLine("test");
             return View();
         }
     }
