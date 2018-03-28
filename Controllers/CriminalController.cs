2188150fa6eb2bdb199905d58ddd5a758f597dc4
Ryan Graute <rrgraute@gmail.com>
2018-03-28 16:25:40 +0200

    get districts in CriminalController

--- Controllers/CriminalController.cs
+++ Controllers/CriminalController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using project3data.Data;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,9 +12,19 @@ namespace project3data.Controllers
 {
     public class CriminalController : Controller
     {
+        private readonly DatabaseContext database;
+
+        public CriminalController(DatabaseContext context)
+        {
+            database = context;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
+            string json = JsonConvert.SerializeObject(this.database.Districts);
+            ViewData["districts"] = json;
+            
             return View();
         }
     }
